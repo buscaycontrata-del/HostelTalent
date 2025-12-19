8bd0c4c11a89e4cdd64d368f76cc2e2a72db0445
buscaycontrata-del <buscaycontrata@gmail.com>
2025-12-19 12:46:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+// Formulario de candidatos
 document.getElementById("candidato").addEventListener("submit", e => {
   e.preventDefault();
-  alert("Formulario enviado correctamente (esto es una prueba)");
+  alert("Formulario de candidato enviado correctamente (esto es una prueba)");
+});
+
+// Formulario de empresas
+document.getElementById("empresa").addEventListener("submit", e => {
+  e.preventDefault();
+  alert("Formulario de empresa enviado correctamente (esto es una prueba)");
 });
